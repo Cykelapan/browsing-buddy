dc7a3105828d3ab20b485ab9b07782d78f9c5585
Cykelapan <147327743+Cykelapan@users.noreply.github.com>
2025-03-09 13:27:49 +0100

    Setup
    Empty x-code project for iOS

---
+++ browsing-buddy/browsing-buddy/browsing_buddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  browsing_buddyApp.swift
+//  browsing-buddy
+//
+//  Created by Frida Granlund on 2025-03-09.
+//
+
+import SwiftUI
+
+@main
+struct browsing_buddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ browsing-buddy/browsing-buddyTests/browsing_buddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  browsing_buddyTests.swift
+//  browsing-buddyTests
+//
+//  Created by Frida Granlund on 2025-03-09.
+//
+
+import Testing
+@testable import browsing_buddy
+
+struct browsing_buddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
