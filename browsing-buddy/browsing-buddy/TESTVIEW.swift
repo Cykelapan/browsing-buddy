8b132405cea8978a99e889f6ac813f6063e75501
Cykelapan <147327743+Cykelapan@users.noreply.github.com>
2025-03-14 22:43:38 +0100

    update

--- browsing-buddy/browsing-buddy/TESTVIEW.swift
+++ browsing-buddy/browsing-buddy/TESTVIEW.swift
@@ -11,22 +11,37 @@ import SwiftUI
 
 //För settings av användare
 // Textstorlek, Färg?, Favoriter
-
+//https://www.swiftanytime.com/blog/form-in-swiftui
 struct TESTVIEW: View {
     @State private var fontSize: CGFloat = 15
     @State private var firstName: String = ""
     @State private var lastName: String = ""
-    
+    @State private var date = Date()
+    @State private var darkmode : Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
         VStack{
-            LabeledContent("iOS Version", value: "2.2.1")
-            Section("Notes Font Size \(Int(fontSize))") {
-                Slider(value: $fontSize, in: 10...40, step: 1) {
-                    Text("Point Size \(Int(fontSize))")
+            Form{
+                Section("Personal Information") {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                
+                        
+                    DatePicker(
+                        "Birthdate",
+                        selection: $date,
+                        displayedComponents: [.date]
+                    )
+                }
+                Section("Font Size \(Int(fontSize))") {
+                    Slider(value: $fontSize, in: 10...40, step: 1) {
+                        Text("Point Size \(Int(fontSize))")
+                    }
+                }
+                Section("OTHER"){
+                    Toggle("Darkmode", isOn: $darkmode)
                 }
             }
-            
         }
         
     }
