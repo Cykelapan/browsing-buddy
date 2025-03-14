b64bef9df97cc892715b70bcfa7e4697acbd7174
Cykelapan <147327743+Cykelapan@users.noreply.github.com>
2025-03-14 21:04:27 +0100

    Update WebCommand.swift

--- browsing-buddy/browsing-buddy/Pre-engine/Models/WebCommand.swift
+++ browsing-buddy/browsing-buddy/Pre-engine/Models/WebCommand.swift
@@ -12,23 +12,39 @@ enum ElementKeyToUse : String, Codable{
     case X_PATH = "xPath" //document.evaluate() -> //
     case CLASS_NAME = "className" // document.getElementsByClassName(className);
     case CSS_SELECTOR = "cssSelector" // document.querySelector()
+    case NO_KEY = "noKeyNeeded"
 }
+
 
 enum FunctionToCall : String, Codable {
-    case INPUT_REQUEST = "INPUT_REQUEST"
-    case SHOW_MESSAGE = "SHOW_MESSAGE"
+    case INPUT_REQUEST = "INPUT_REQUEST"//no key just input
+    case SHOW_MESSAGE = "SHOW_MESSAGE" //no key just show
     case SHOW_EXTRACTED_MESSAGE = "SHOW_EXTRACTED_MESSAGE"
-    case CLICK_BUTTON = "CLICK_BUTTON"
-    case Extract_Message = "Extract_Message"
-    case Insert_Element = "Insert_Element"
-    case Insert_Element_Class = "Insert_Element_Class"
-    case A = "A"
-    case D = "D"
-    case G = "G"
+    case Extract_Message = "Extract_Message" //xPath
+    case Insert_Element = "Insert_Element" //xPath
+    case Insert_Element_Class = "Insert_Element_Class" //className
+    case Extract_List_By_Xpath = "Extract_List_By_Xpath" //xpath
+    case A = "A" // want url
+    case D = "D" // className
+    case G = "G" // xPath
+    
+    func getValue(jsKey: JSElementKeys) -> String?{
+        switch self {
+        case .INPUT_REQUEST, .SHOW_MESSAGE, .SHOW_EXTRACTED_MESSAGE ,.A:
+            return "Kvittar"
+            
+        case .Insert_Element_Class, .D:
+            return jsKey.classPath
+            
+        case .Extract_Message, .Insert_Element, .Extract_List_By_Xpath, .G:
+            return jsKey.xPath
+            
+        }
+    }
 }
 
-enum ParameterAction : Int, Codable{
-    case NOTHING = 0
+enum ExtractFromUser : Int, Codable{
+    case NOTHING = 1
     case GET_USERNAME
     case GET_EMAIL
     case GET_PASSWORD
@@ -65,27 +81,41 @@ enum ParameterAction : Int, Codable{
 //Lägg till elementType, elementKey enbart en string
 struct WebCommand : Codable{
     let functionToCall : FunctionToCall
+    let extractFromUser : String
+    let informationTitle : String?
+    
+    let willNavigate : Bool
+    let websiteUrl : String //bara url med path
+    
     let jsElementKeys : JSElementKeys
     let jsElementKey : String
-    var jsInputParameter : String?
-    let parameterAction : ParameterAction
-    let willNavigate : Bool
+    var jsInputValue : String?
     
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.functionToCall = try container.decode(FunctionToCall.self, forKey: .functionToCall)
         self.jsElementKeys = try container.decode(JSElementKeys.self, forKey: .jsElementKeys)
-        self.jsInputParameter = try container.decode(String?.self, forKey: .jsInputParameter)
+        self.jsInputValue = try container.decode(String.self, forKey: .jsInputValue)
         self.willNavigate = try container.decode(Bool.self, forKey: .willNavigate)
-        self.parameterAction = try container.decode(ParameterAction.self, forKey: .parameterAction)
-        self.jsElementKey = ""
+        let extract = try container.decode(ExtractFromUser.self, forKey: .extractFromUser)
+        self.informationTitle = try container.decode(String?.self, forKey: .informationTitle)
+        self.websiteUrl = try container.decode(String.self, forKey: .websiteUrl)
+        
+        self.jsInputValue = extract.getValue() ?? ""
+        self.extractFromUser = extract.getValue()
+        guard let key =  self.functionToCall.getValue(jsKey: self.jsElementKeys) else {
+            fatalError("missing key")
+        }
+        self.jsElementKey = key
         //TODO: fixa så det väljer nyckel baserat på det som ska användas
     }
 }
 struct JSElementKeys: Codable {
     let className : String?
+    let classPath : String?
     let elementId : String?
     let elementTag : String?
     let cssSelector : String?
     let xPath : String?
+    //let cssSelector : String?
 }
